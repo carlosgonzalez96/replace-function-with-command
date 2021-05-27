b7013c72e5e2692a1ea96207d62256b8dfc3d1a7
carlosgonzalez96 <carlosglez817@gmail.com>
2021-05-26 21:33:24 -0700

    refactored funciton

--- replaceFunctionWithCommand.js
+++ replaceFunctionWithCommand.js
@@ -1,16 +1,38 @@
 function isBeeLethal(amtOfBees) {
-    let venomAmt = 0.3 * 100;
-    let beeAmt = amtOfBees;
-    var totalAmtStings = 0;    
+    return new Bee(amtOfBees).execute();
 
-    totalAmtStings = (venomAmt * beeAmt) / 100
+}
 
-    if(totalAmtStings > 55) {
-        return console.log("total 🐝 stings: " + amtOfBees + ", 🧪 venom: " + totalAmtStings + "mg, result: 💀");
-    } else {
-        return console.log("total 🐝 stings: " + amtOfBees + ", 🧪 venom: " + totalAmtStings + "mg, result: you are ok if not allergic");
+class Bee {
+    constructor(amtOfBees) {
+        this._amtOfBees = amtOfBees;
+    }
+
+    execute() {
+        this._venomAmt = 0.3 * 100;
+        this._beeAmt = this._amtOfBees;
+        this._totalAmtStings = 0;    
+
+        this._totalAmtStings = (this._venomAmt * this._beeAmt) / 100
+
+        //lots more code
+
+        this.printVenomInfo();
+    }
+
+    printVenomInfo() {
+    return console.log(`total 🐝 stings: ${this._amtOfBees}, 🧪 venom: ${this._totalAmtStings}mg, result: ${this.result()}`);
+    }
+
+    result() {
+        if(this._totalAmtStings > 55) {
+            return "💀";
+        } else {
+            return "you are ok if not allergic";
+        }
     }
 
 }
 
-isBeeLethal(188);
+isBeeLethal(200)
+isBeeLethal(2)
